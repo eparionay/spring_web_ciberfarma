a6fed54fbac415083bc8d38f952bc6f40820f060
E <e@prueba.com>
2025-10-30 15:05:13 -0500

    e

--- src/test/java/com/ciberfarma/spring_jpa_crud/SpringJpaCrudApplicationTests.java
+++ src/test/java/com/ciberfarma/spring_jpa_crud/SpringJpaCrudApplicationTests.java
@@ -3,7 +3,8 @@ package com.ciberfarma.spring_jpa_crud;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = com.ciberfarma.spring_spring_ciberfarma.SpringJpaCrudApplication.class)
+
 class SpringJpaCrudApplicationTests {
 
 	@Test
